a8f0260acc170b4c9beb2a1de82986233baf90aa
Katrina <99131087+notkatrina@users.noreply.github.com>
2023-08-18 17:19:58 -0500

    Create 08-18-2023-apecoin.pm

---
+++ 08-18-2023-apecoin.pm
@@ -0,0 +1,12 @@
+We are contacting you as the host of content (the “Infringing Content”) offered on your platform which infringes on the copyright owned by Ape Foundation. See Exhibit A. We request that you remove the Infringing Content as soon as possible. The necessary information for this takedown request is as follows:
+Protected Works:
+https://apecoin[.]com/
+
+Infringing Content/URLs:
+apecoin[.]pm
+
+Additional Information:
+The Infringing Content uses copyrighted material from https://apecoin[.]com/
+produced by Ape Foundation without authorization.
+
+https://netlify.zendesk.com/agent/tickets/176493
